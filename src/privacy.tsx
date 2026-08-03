35ed43ab75ed522d70bba81a3c3f72af9da95f71
Nicolas H. <nico.the.tall@proton.me>
2026-08-03 02:17:15 +0200

    re-adapted privacy page

--- src/privacy.tsx
+++ src/privacy.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "@/App";
+import { Privacy } from "@/pages/privacy/Privacy";
+import "@/global.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Privacy />
   </StrictMode>,
 );
