b2e16d733bf62a4f8d320e6541ad37a5dd7fde4e
Nicolas H. <nico.the.tall@proton.me>
2026-08-09 04:52:44 +0200

    close a11y window on outside clicks

--- src/components/ButtonContainer/ButtonContainer.tsx
+++ src/components/ButtonContainer/ButtonContainer.tsx
@@ -1,4 +1,4 @@
-import { useState, type JSX } from "react";
+import { useEffect, useRef, useState, type JSX } from "react";
 import sx from "@/components/ButtonContainer/ButtonContainer.module.scss";
 
 interface Props {
@@ -8,9 +8,28 @@ interface Props {
 
 export function ButtonContainer({ text, children }: Props) {
   const [isOpen, setIsOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!containerRef.current?.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-    <div className={sx.button_container}>
+    <div className={sx.button_container} ref={containerRef}>
       {isOpen && <div className={sx.container}>{children}</div>}
       <button
         onClick={() => {
