aa12d897e42d61c6a8fa1a6c62c52b952f3f4b83
Nicolas H. <nico.the.tall@proton.me>
2026-08-09 07:44:48 +0200

    ui improvements

--- src/components/programs/WebArchive/WebArchive.tsx
+++ src/components/programs/WebArchive/WebArchive.tsx
@@ -3,6 +3,10 @@ import sx from "@/components/programs/WebArchive/WebArchive.module.scss";
 export function WebArchive() {
   return (
     <div className={sx.web_archive}>
+      <small>
+        If this doesn't load or loads slowly, blame Wayback Machine. Donate to
+        the Internet Archive so they can keep being awesome.
+      </small>
       <iframe src="https://web.archive.org/web/20260616201916_/https://nicolas.nekoweb.org/" />
     </div>
   );
