f01d763f7bdd14542ced55e243b4a832f2c6f8b9
Nicolas H. <nico.the.tall@proton.me>
2026-08-09 10:47:04 +0200

    updated euroring widget

--- src/components/webrings/Euro.tsx
+++ src/components/webrings/Euro.tsx
@@ -7,7 +7,7 @@ export function Euro() {
     const container = ref.current!;
     const sources = [
       "https://euroring.neocities.org/scripts/onionring-variables.js",
-      "https://euroring.neocities.org/scripts/euroringbanner.js",
+      "https://euroring.neocities.org/scripts/euroring_large_butt.js",
     ];
 
     sources.forEach((src) => {
